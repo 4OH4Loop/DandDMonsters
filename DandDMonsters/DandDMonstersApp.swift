ef000f2d520ab9d10e4ca0b187073d343fd92b4f
4OH4Loop <semperchuck@gmail.com>
2025-04-14 18:13:03 -0400

    Initial Commit

---
+++ DandDMonsters/DandDMonstersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DandDMonstersApp.swift
+//  DandDMonsters
+//
+//  Created by Carolyn Ballinger on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct DandDMonstersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
